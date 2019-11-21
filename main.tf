50344520efbac5ed219e7eecebc5ff5374d7afd9
venkateshtammali <39961342+venkateshtammali@users.noreply.github.com>
2019-11-21 13:41:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,5 +64,7 @@ resource "aws_kinesis_firehose_delivery_stream" "test_stream" {
   s3_configuration {
     role_arn = "${aws_iam_role.firehose_role.arn}"
     bucket_arn = "${aws_s3_bucket.bucket.arn}"
+    buffer_size = 3
+    buffer_interval = 60
   }
 }
